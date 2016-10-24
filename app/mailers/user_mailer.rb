96ac42bcbc414b2972cb7e16d6d5c70cf51bd260
Nisarg10 <nisarg.desai@mavs.uta.edu>
2016-10-24 12:10:10 -0700

    when comment is made on post, email is sent to post owner.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,14 @@
 class UserMailer < ApplicationMailer
-	default from: "kishore.batch35@gmail.com"
+	default from: "desainisarg10@gmaicurrenl.com"
 
 	def registration_mailer(post,user)
 		@user = user
 		@post = post
 		mail(to: @user.email, subject: "Thanks for your post")
+	end
+	def comment_mailer(post,comment)
+		@post = post
+		@post_user = User.find(Post.find(@post.id).user_id)
+		mail(to: @post_user.email, subject: "Somebody commented on your post")
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020024014) do
+ActiveRecord::Schema.define(version: 20161024180503) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "comment"
@@ -19,12 +19,14 @@ ActiveRecord::Schema.define(version: 20161020024014) do
     t.datetime "updated_at", null: false
     t.integer  "post_id"
     t.integer  "reply_id"
+    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
