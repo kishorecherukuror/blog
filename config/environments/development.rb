3e83b99fe283d637256d7ab8b0ab3226101d3fae
kishore <kishorecheruku.ror@gmail.com>
2016-10-24 08:05:16 +0530

    Mailer is done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
  :user_name            => "kishore.batch35@gmail.com",
- :password             => "kishorekrish",
+ :password             => "xxxxxxxxxxxxx",
  :authentication       => "plain",
  :enable_starttls_auto => true
 }
